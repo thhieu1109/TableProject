0f0e6600b6f0ce81344b6f364ee2480922882cbf
thhieu1109 <qqqhieubbb@gmail.com>
2025-06-13 18:26:21 +0700

    Some responsive or Mobile after table code push

--- js/home.js
+++ js/home.js
@@ -1,17 +1,32 @@
-const getItem = document.querySelectorAll(".item")
-const left_icon = document.querySelector(".left-icon")
-const hideMenu = document.querySelector(".menu")
+// Biến lấy các item trong menu sidebar
+const getItem = document.querySelectorAll(".item");
 
-left_icon.addEventListener("click", () => {
-    getItem.forEach(e => e.classList.toggle("d-none"))
-    hideMenu.classList.toggle("none")
-})
+// Biến cha để lấy 2 icon bên trong (icon thu gọn/mở rộng sidebar)
+const leftIcon = document.querySelector(".left-icon");
 
+// Biến con để lấy 2 icon đổi qua lại
+const iconLeft = leftIcon.querySelector(".fa-arrow-left");
+const iconRight = leftIcon.querySelector(".fa-arrow-right-to-bracket");
 
+// Biến để ẩn/hiện toàn bộ menu
+const hideMenu = document.querySelector(".menu");
 
-const userAvatar = document.getElementById("userAvatar")
-const userDropdown = document.getElementById("userDropdown")
+leftIcon.addEventListener("click", () => {
+    // Ẩn/hiện item bên menu khi bấm icon
+    getItem.forEach(e => e.classList.toggle("d-none"));
+    hideMenu.classList.toggle("none");
+
+    // Đổi icon
+    iconLeft.classList.toggle("d-none");
+    iconRight.classList.toggle("d-none");
+});
+
+// Dropdown user info khi bấm vào avatar
+// Biến lấy avatar của user bằng DOM
+const userAvatar = document.getElementById("userAvatar");
+// Biến lấy dropdown của user bằng DOM(hiện đang bị ẩn do thuộc tính d-none của Boostrap)
+const userDropdown = document.getElementById("userDropdown");
 
 userAvatar.addEventListener("click", () => {
-    userDropdown.classList.toggle("d-none")
-})
+    userDropdown.classList.toggle("d-none");
+});
